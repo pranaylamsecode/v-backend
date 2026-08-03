f276175275b86a77a69447d2d2e4ac132dfe9db5
pranay-lamse <pranay.l@veerit.com>
2026-08-03 08:28:25 +0530

    fix: trust Vercel proxy IPs to prevent false 429 Too Many Requests rate limiting

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->prepend(\App\Http\Middleware\ApiTokenMiddleware::class);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
